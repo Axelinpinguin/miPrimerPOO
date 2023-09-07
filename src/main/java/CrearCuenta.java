7b4fb0fd63b136d08052daa111645b13c7e647b0
= <=>
2023-09-07 11:32:01 -0600

    modificacion axel

--- src/main/java/CrearCuenta.java
+++ src/main/java/CrearCuenta.java
@@ -4,5 +4,6 @@ public class CrearCuenta {
         System.out.printf("Saldo de la cuenta: %.2f\n", cuentaDeVanya.saldo);
         cuentaDeVanya.saldo=10000;
         System.out.println("Saldo de la cuenta: " + cuentaDeVanya.saldo);
+        System.out.println("hola");
     }
 }
